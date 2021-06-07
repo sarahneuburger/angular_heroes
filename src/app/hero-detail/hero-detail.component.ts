eb856815afade45922432b5b6eefb8e0a3d6b371
Sarah Neuburger <sarahneuburger94@gmail.com>
2021-06-07 16:22:26 -0300

    Etapa 3 - ok

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+// Import da interface Hero
+import { Hero } from '../hero';
+
+
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,6 +10,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+
+  // Atributo para aparesentar dados, ligada ao html
+  @Input() hero?: Hero;
 
   constructor() { }
 
